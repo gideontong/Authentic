2d5303968bfde9930f5398e716fe7fbdc314d18b
Gideon Tong <funnygid@gmail.com>
2018-12-01 00:16:12 -0800

    Create the banner and MVP

--- src/src/inject/inject.js
+++ src/src/inject/inject.js
@@ -9,11 +9,28 @@ chrome.extension.sendMessage({}, function(response) {
 		// ----------------------------------------------------------
 
 		// Check if someone has browsed to infowars.com
-		var url = window.location.href;
+		var url = window.location.hostname; // Get hostname
 		console.log(url);
-		if(url == "https://www.infowars.com/") {
+		
+		// Create the banner
+		if(url == "www.infowars.com") {
 		  console.log("It works!");
+		  var banner = document.createElement('div');
+		  banner.id="banner";
+		  banner.style.background = "#F00";
+		  banner.style.width="100%";
+		  banner.style.height="200px";
+		  banner.style.position="fixed";
+		  banner.style.top="20px";
+		  document.body.appendChild(banner);
+		  var text = document.createElement('h1');
+		  text.innerHTML = "This is a sketch article!";
+		  text.style.fontSize = "65px";
+		  text.style.color = "white";
+		  banner.appendChild(text)
+  
 		}
+
 
 	}
 	}, 10);
