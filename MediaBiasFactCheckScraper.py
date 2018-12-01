9905722a0b1d8a82bdba88d48c6b325589ea1bc8
Gideon Tong <funnygid@gmail.com>
2018-11-30 22:16:59 -0800

    Initial scraper code

--- MediaBiasFactCheckScraper.py
+++ MediaBiasFactCheckScraper.py
@@ -1,1 +1,9 @@
 import csv
+import requests
+from bs4 import BeautifulSoup
+
+// Creating the URL
+test_url = "https://mediabiasfactcheck.com/new-york-times/"
+page = requests.get(test_url)
+
+soup = BeautifulSoup(page.text, 'html.parser')
